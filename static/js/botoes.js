ad05253c1979a60660ab768cc126dada56a8f15e
JotaPe_Lopes <jpaullopes27@gmail.com>
2025-05-10 08:00:13 -0300

    edited: buttons files

--- static/js/botoes.js
+++ static/js/botoes.js
@@ -1,20 +1,25 @@
 document.addEventListener("DOMContentLoaded", (evento) => {
-    const socket = io(); // Conecta ao servidor SocketIO na mesma origem
+    const socket = io(); // 1. Conecta no servidor (o recepcionista lá do app.py)
 
-    socket.on("novo_dado", function (dado) {
-        // Verifica se os dados dos botões existem antes de tentar acessá-los
-
+    socket.on("novo_dado", function (dado) { // 2. Fica escutando a mensagem "novo_dado"
+        // Pega os lugares no HTML onde a informação vai aparecer
         const statusBotaoAElement = document.getElementById("status_botao_a");
         const statusBotaoBElement = document.getElementById("status_botao_b");
 
+        // 3. Verifica se a informação recebida é sobre os botões
         if (dado && typeof dado.botao_a !== 'undefined') {
+            // Se o 'dado' tem 'botao_a', atualiza o Botão A
             statusBotaoAElement.innerText = dado.botao_a == 1 ? "Pressionado!" : dado.botao_a == 0 ? "Solto" : "N/A";
         } else {
+            // Se não tem informação do Botão A no 'dado' atual
             statusBotaoAElement.innerText = "N/A";
         }
+
         if (dado && typeof dado.botao_b !== 'undefined') {
+            // Se o 'dado' tem 'botao_b', atualiza o Botão B
             statusBotaoBElement.innerText = dado.botao_b == 1 ? "Pressionado!" : dado.botao_b == 0 ? "Solto" : "N/A";
         } else {
+            // Se não tem informação do Botão B no 'dado' atual
             statusBotaoBElement.innerText = "N/A";
         }
     });
